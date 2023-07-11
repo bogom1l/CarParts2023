5e80a1bcacacb0ed1751f36f745302111d81337a
wavezM5 <102171820+wavezM5@users.noreply.github.com>
2023-07-11 09:50:46 +0300

    Initial commit

---
+++ CarParts2023/CarParts2023/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace CarParts2023.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ CarParts2023/CarParts2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarParts2023.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
