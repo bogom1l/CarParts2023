1704434915e2aa0d2882cfdcc9876aeb9a8e7fe8
wavezM5 <102171820+wavezM5@users.noreply.github.com>
2023-08-07 13:58:08 +0300

    implement auto mapping

--- CarParts/CarParts.Services.Data/RentService.cs
+++ CarParts/CarParts.Services.Data/RentService.cs
@@ -2,6 +2,7 @@
 {
     using CarParts.Data;
     using Interfaces;
+    using Mapping;
     using Microsoft.EntityFrameworkCore;
     using Web.ViewModels.Rent;
 
@@ -21,16 +22,8 @@
                 .Include(c => c.Dealer.User)
                 .Include(c => c.Renter)
                 .Where(c => c.RenterId != null)
-                .Select(c => new RentInfoViewModel
-                {
-                    CarMake = c.Make,
-                    CarModel = c.Model,
-                    CarImageUrl = c.ImageUrl,
-                    DealerFullName = c.Dealer.User.FirstName + " " + c.Dealer.User.LastName,
-                    DealerEmail = c.Dealer.User.Email,
-                    RenterFullName = c.Renter!.FirstName + " " + c.Renter.LastName,
-                    RenterEmail = c.Renter.Email
-                }).ToListAsync();
+                .To<RentInfoViewModel>()
+                .ToListAsync();
 
             return allRents;
         }
--- CarParts/CarParts.Web.ViewModels/Rent/RentInfoViewModel.cs
+++ CarParts/CarParts.Web.ViewModels/Rent/RentInfoViewModel.cs
@@ -1,6 +1,11 @@
 ﻿namespace CarParts.Web.ViewModels.Rent
 {
-    public class RentInfoViewModel
+    using AutoMapper;
+    using Common;
+    using Data.Models;
+    using Services.Mapping;
+
+    public class RentInfoViewModel : IMapFrom<Car>, IHaveCustomMappings
     {
         public string CarMake { get; set; } = null!;
         public string CarModel { get; set; } = null!;
@@ -14,5 +19,24 @@
         public string RenterFullName { get; set; } = null!;
 
         public string RenterEmail { get; set; } = null!;
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration
+                .CreateMap<Car, RentInfoViewModel>()
+                .ForMember(d => d.CarMake, opt =>
+                    opt.MapFrom(s => s.Make))
+                .ForMember(d => d.CarModel, opt =>
+                    opt.MapFrom(s => s.Model))
+                .ForMember(d => d.CarImageUrl, opt =>
+                    opt.MapFrom(s => s.ImageUrl))
+                .ForMember(d => d.DealerFullName, opt => 
+                    opt.MapFrom(s => s.Dealer.User.FirstName + " " + s.Dealer.User.LastName))
+                .ForMember(d => d.DealerEmail,
+                    opt => opt.MapFrom(s => s.Dealer.User.Email))
+                .ForMember(d => d.RenterFullName, opt =>
+                    opt.MapFrom(s => s.Renter!.FirstName + " " + s.Renter!.LastName))
+                .ForMember(d => d.RenterEmail, opt =>
+                    opt.MapFrom(s => s.Renter!.Email));
+        }
     }
 }
